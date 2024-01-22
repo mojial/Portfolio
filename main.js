775c10aaca1ddd90a55438728063f60bfcb67ea4
mojial <mojial@floridauniversitaria.es>
2024-01-22 10:22:19 +0100

    pequeño arreglo del javascript

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 const imagenIdioma = document.querySelector('.bandera-mini');
 const textoLenguaje = document.querySelector('.toggleLenguage');
 
-let idiomaActual = "ESP";
+let idiomaActual = "ES";
 
 console.log(textoLenguaje);
 
 textoLenguaje.addEventListener('click', () => {
-    if (idiomaActual === "ESP") {
+    if (idiomaActual === "ES") {
         // Cambiar a inglés
-        textoLenguaje.innerHTML = "ENG";
+        textoLenguaje.innerHTML = "EN";
         imagenIdioma.src = "imagenes/engbandera.png";
-        idiomaActual = "ENG";
+        idiomaActual = "EN";
         /* window.location.href = "index-eng/indexeng.html"; */
     } else {
         // Cambiar a español
-        textoLenguaje.innerHTML = "ESP";
+        textoLenguaje.innerHTML = "ES";
         imagenIdioma.src = "imagenes/spain.png"; 
-        idiomaActual = "ESP";
+        idiomaActual = "ES";
     }
 });
 
 imagenIdioma.addEventListener('click', () => {
-    if (idiomaActual === "ESP") {
+    if (idiomaActual === "ES") {
         // Cambiar a inglés
-        textoLenguaje.innerHTML = "ENG";
+        textoLenguaje.innerHTML = "EN";
         imagenIdioma.src = "imagenes/engbandera.png";
-        idiomaActual = "ENG";
+        idiomaActual = "EN";
         /* window.location.href = "index-eng/indexeng.html"; */
     } else {
         // Cambiar a español
-        textoLenguaje.innerHTML = "ESP";
+        textoLenguaje.innerHTML = "ES";
         imagenIdioma.src = "imagenes/spain.png";
-        idiomaActual = "ESP";
+        idiomaActual = "ES";
     }
 });
